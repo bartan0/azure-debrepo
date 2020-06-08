35b57c36787901ffde22795ceb45403e49a94af1
Bartłomiej Sługocki <0@bartan0.pl>
2020-06-09 00:34:06 +0200

    gpg module uses gpgme

--- azure_debrepo/gpg.py
+++ azure_debrepo/gpg.py
@@ -1,36 +1,80 @@
-import subprocess
-
-from os import path, makedirs, environ
+from gpgme import (
+	PROTOCOL_OpenPGP,
+	SIG_MODE_CLEAR,
+	Context
+)
+from os import chmod, makedirs, path
 
 
 class GPG:
 
-	def __init__ (self, homedir, user_id):
-		self._user_id = user_id
-		self._homedir = path.abspath(homedir)
-		makedirs(self._homedir, 0o700, True)
+	def __init__ (self,
+		homedir,
+		keyfpr = None
+	):
+		gpgbin = path.join(homedir, 'gpg')
+		ctx = self._context = Context()
+		self._fpr = keyfpr
+		self._key = None
 
-	def _gpg (self, *args, **kwarg):
-		return subprocess.run([ 'gpg', *args ],
-			env = {
-				**environ,
-				'GNUPGHOME': self._homedir
-			},
-			**kwarg
-		)
+		self.passphrase = None
 
-	def genkey (self):
-		self._gpg('--quick-gen-key', '--batch', '--passphrase', '', self._user_id,
-			capture_output = True
-		)
+		if not path.exists(homedir):
+			makedirs(homedir, 0o700)
 
-	def pubkey (self, stream):
-		self._gpg('--export', self._user_id,
-			stdout = stream
-		)
+			open(path.join(homedir, 'gpg-agent.conf'), 'w').writelines(
+				l + '\n' for l in [
+					'default-cache-ttl 0'
+				]
+			)
 
-	def clearsign (self, data, stream):
-		self._gpg('--clearsign',
-			input = data,
-			stdout = stream
-		)
+			open(gpgbin, 'w').writelines(l + '\n' for l in [
+				'#!/bin/bash',
+				'exec gpg --pinentry-mode loopback "$@"'
+			])
+
+			chmod(gpgbin, 0o700)
+
+		ctx.set_engine_info(PROTOCOL_OpenPGP, gpgbin, homedir)
+		ctx.passphrase_cb = self._passphrase_cb
+
+	def _get_key (self):
+		if self._key is None:
+			self._key = self._context.get_key(self._fpr)
+
+		return self._key
+
+	def _passphrase_cb (self, *args):
+		open(args[3], 'w').write((self.passphrase or '') + '\n')
+
+		return 0
+
+	def genkey (self, passphrase, *,
+		name = '',
+		comment = '',
+		email = ''
+	):
+		keyinfo = self._context.genkey('\n'.join([
+			'<GnupgKeyParms format="internal">',
+				'Key-Type: RSA',
+				'Key-Length: 4096',
+				'Passphrase: ' + passphrase,
+				'Name-Real: ' + name if name else '',
+				'Name-Comment: ' + comment if comment else '',
+				'Name-Email: ' + email if email else '',
+				'Expire-Date: 730',
+			'</GnupgKeyParms>'
+		]))
+
+		self._fpr = keyinfo.fpr
+
+	def clearsign (self, istream, ostream):
+		self._context.signers = [ self._get_key() ]
+		self._context.sign(istream, ostream, SIG_MODE_CLEAR)
+
+	def pubkey (self,
+		ostream,
+		armor = False
+	):
+		self._context.armor = armor
+		self._context.export(self._fpr, ostream)
